c72cbc123b095f4d94e5d1980f0b1a32663d0d64
sayeed083 <sayeedislam407@gmailcom>
2023-06-05 10:25:21 +0600

    Making A User Admin and Delete a User Operation Done

--- src/Pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/Pages/DashBoard/AllUsers/AllUsers.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { FaTrashAlt, FaUserShield } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
@@ -9,11 +10,52 @@ const AllUsers = () => {
         return res.json();
     })
 
-    const handleUpdateUserRole = id => {
-        
+    const handleUpdateUserRole = user => {
+        fetch(`http://localhost:5000/users/admin/${user._id}`, {
+            method: 'PATCH'
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.modifiedCount){
+                refetch()
+                Swal.fire({
+                    position: 'top',
+                    icon: 'success',
+                    title: `${user.name} is now Admin`,
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            }
+        })
     }
     const handleDelete = user => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
 
+                fetch(`http://localhost:5000/users/${user._id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            refetch()
+                            Swal.fire(
+                                'Deleted the User!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
     }
 
     return (
@@ -40,7 +82,7 @@ const AllUsers = () => {
                             <td>{user.name}</td>
                             <td>{user.email}</td>
                             <td>{user.role === 'admin' ? 'admin' :
-                                <button onClick={() => handleUpdateUserRole(user._id)} className="btn btn-ghost bg-orange-600 text-white"><FaUserShield></FaUserShield></button>
+                                <button onClick={() => handleUpdateUserRole(user)} className="btn btn-ghost bg-orange-600 text-white"><FaUserShield></FaUserShield></button>
 
                             }</td>
                             <td>
